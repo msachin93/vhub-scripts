f9ff536fb95104ba9fcd6f3f0687b1cce710e5bd
Sachin <sachin@vhub.ai>
2024-08-25 18:46:19 +0530

    small change

--- instagram.py
+++ instagram.py
@@ -15,8 +15,8 @@ class Scraper():
                         'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)'
                         }
         self.user = user
-        self.url_prefix = "http://127.0.0.1:5000/vedasis/scraping"
-        # self.url_prefix = "https://vhub-admin-backend-bsng2qeg2a-em.a.run.app/vedasis/scraping"
+        # self.url_prefix = "http://127.0.0.1:5000/vedasis/scraping"
+        self.url_prefix = "https://vhub-admin-backend-bsng2qeg2a-em.a.run.app/vedasis/scraping"
 
         self.profile_pics = []
         self.iterations = iters
